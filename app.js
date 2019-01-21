dc0e0de174424ef06c72d009e3b4e42a87a3aaeb
MaloDrougard <malcommalo.drougard@epfl.ch>
2019-01-21 16:51:38 +0100

    working chat

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ console.log("project start")
 
 // the import /require
 var express = require('express');
-var bodyParser = require('body-parser');
 var path = require('path'); // path is core module of node, (no need of install)
 
 var app = express(); 
@@ -20,28 +19,6 @@ var logger = function(req, res, next) {
 app.use(logger); 
 */
 
-var users = [
-  {
-    id: 1,
-    name: "prout",
-    action: "a1"
-  },
-   {
-    id: 2,
-    name: "dsfa",
-    action: "a2"
-  },
-   {
-    id: 3,
-    name: "proutuoupt",
-    action: "a3"
-  },
-   {
-    id: 4,
-    name: "proufdat",
-    action: "a4"
-  }
-]
 
 
 // Set the middleware view engine
@@ -49,27 +26,26 @@ app.set('view engine', 'ejs');
 // dir that contains the view templates
 app.set('views', path.join(__dirname, 'views')); 
 
-// set the body-parser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-
 // set and serve static path
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req,res){
-  res.render("index",{users: users}  ); 
-});
-
-app.post('/users/add', function(req,res) {
-  var newUser = {
-    id: req.body.id,
-    name: req.body.name,
-    action: req.body.action
-  }
-  users.push(newUser); 
+  res.render("index", {appTitle: "chat effect"} ); 
 });
 
 
-app.listen(3000, function() {
+var server = app.listen(3000, function() {
   console.log("Server started on port 3000;"); // call back function
 }) ;
+
+var io = require('socket.io')(server)
+io.on('connection' , function(socket) {
+  console.log("new user connected"); 
+  
+  socket.on('push_new_message', function(data) {
+    console.log("new message recived: val=" + data.message); 
+    // broadcast the message (remark the s of sockets)
+    io.sockets.emit('fire_new_message', {message: data.message} );
+  });
+  
+});
---
+++ public/chat.js
@@ -0,0 +1,19 @@
+$(function(){
+  var socket = io.connect('http://localhost:3000')
+  
+  var submitBtn = $("#submitBtn");
+  var textInput = $("#textInput");
+  var chatRoom = $("#room"); 
+  
+  
+  submitBtn.click(function () {
+    console.log("Submit btn clicked with value: " + textInput.text()); 
+    socket.emit('push_new_message', {message: textInput.text() } );   
+  });
+  
+  socket.on('fire_new_message', function (data) {
+    console.log("new message recived" + data.message);
+    chatRoom.append("<p>" + data.message + "</p>");
+  });
+  
+}); 
